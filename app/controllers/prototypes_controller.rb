70757d8eb96ad539bda87647fca6464814fc0d1c
森田一 <moritahajime@moritahajimenoMacBook-Air.local>
2020-11-19 16:55:55 +0900

    機能の修正
    機能の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -22,17 +22,14 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    
     @comment = Comment.new
     @comments = @prototype.comments.includes(:user)
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
       redirect_to prototype_path
     else
@@ -41,7 +38,7 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
-    prototype.destroy
+    @prototype.destroy
     redirect_to root_path
   end
 
@@ -58,13 +55,14 @@ class PrototypesController < ApplicationController
     end
   end
 
-  def set_prototype
-    @prototype = Prototype.find(params[:id])
-  end
-
+ 
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
   end
 
 end
